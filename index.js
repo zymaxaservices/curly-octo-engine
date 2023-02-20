327000d42916fb4b97777c5de0cc27ceb38a2f23
zymaxaservices <93442335+zymaxaservices@users.noreply.github.com>
2023-02-20 21:42:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ color: green;
 </html>
 `);
           }else{
-            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(`<!DOCTYPE html>
 <html>
 <head>
